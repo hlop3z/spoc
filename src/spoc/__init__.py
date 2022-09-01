e1804e4dfa044d0102433603237c285f9e188f3a
hlop3z <23062270+hlop3z@users.noreply.github.com>
2022-09-01 00:29:50 -0500

    search_method

--- src/spoc/__init__.py
+++ src/spoc/__init__.py
@@ -9,7 +9,7 @@ from .frozendict import FrozenDict as frozendict
 from .components import component
 from .project import project
 from .tools import get_attr, get_fields
-
+from .imports import search_method
 
 def root(path):
     """Easy Level-Up Folder(s)."""
