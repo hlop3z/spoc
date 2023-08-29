1040a8c813319422002a7f5ab55a890ce1617b4f
hlop3z <23062270+hlop3z@users.noreply.github.com>
2023-08-29 14:29:30 -0500

    Update installer.py

--- src/spoc/installer.py
+++ src/spoc/installer.py
@@ -1,7 +1,7 @@
 import pathlib
 from types import SimpleNamespace
 
-SETTINGS = '''
+SETTINGS_TEXT = '''
 # -*- coding: utf-8 -*-
 """
     { Settings }
@@ -13,7 +13,7 @@ import pathlib
 BASE_DIR = pathlib.Path(__file__).parents[1]
 '''
 
-CONFIG = '''
+CONFIG_TEXT = '''
 # -*- coding: utf-8 -*-
 """
     { Config }
@@ -21,6 +21,22 @@ CONFIG = '''
 
 from . import settings
 '''
+
+
+SPOC_TEXT = """
+[spoc]
+mode = "custom" # options: custom, development, production, staging
+custom_mode = "development"
+
+# Modes
+[spoc.apps]
+production = []
+development = []
+staging = []
+
+# Extras
+[spoc.extras]
+"""
 
 
 def create_base():
@@ -32,13 +48,18 @@ def create_base():
     install_helper.config_dir = root_dir / "config"
     install_helper.config_file = install_helper.config_dir / "__init__.py"
     install_helper.settings_file = install_helper.config_dir / "settings.py"
+    install_helper.spoc_file = install_helper.config_dir / "spoc.toml"
 
     install_helper.config_dir.mkdir(exist_ok=True)
 
     if not install_helper.config_file.exists():
-        with open(install_helper.config_file, "w", encoding="utf8") as file:
-            file.write(CONFIG)
+        with open(install_helper.config_file, "w", encoding="utf-8") as file:
+            file.write(CONFIG_TEXT)
 
     if not install_helper.settings_file.exists():
-        with open(install_helper.settings_file, "w", encoding="utf8") as file:
-            file.write(SETTINGS)
+        with open(install_helper.settings_file, "w", encoding="utf-8") as file:
+            file.write(SETTINGS_TEXT)
+
+    if not install_helper.spoc_file.exists():
+        with open(install_helper.spoc_file, "w", encoding="utf-8") as file:
+            file.write(SPOC_TEXT)
