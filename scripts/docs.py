f2fc1f14c7aa31ea7fa592a234a3803ac2dbd338
hlop3z <23062270+hlop3z@users.noreply.github.com>
2024-08-28 15:36:34 -0500

    Update docs.py

--- scripts/docs.py
+++ scripts/docs.py
@@ -6,7 +6,6 @@ from functools import partial
 # Define commands for various operations
 CMD_DEV = "python -m mkdocs serve --dev-addr 0.0.0.0:8056"
 CMD_DEPLOY = "python -m mkdocs gh-deploy --force"
-CMD_MIKE = "mike"
 
 
 def execute_command(docs_dir: Path, command: str) -> None:
@@ -23,16 +22,6 @@ def get_args() -> argparse.Namespace:
     parser.add_argument(
         "-gh", "--github", action="store_true", help="Deploy to GitHub pages"
     )
-    parser.add_argument(
-        "-m", "--mike", action="store_true", help="Use Mike for versioning"
-    )
-    parser.add_argument(
-        "-r", "--reset", action="store_true", help="Reset Mike versioning"
-    )
-    parser.add_argument("-l", "--list", action="store_true", help="Mike list versions")
-    parser.add_argument(
-        "-sd", "--set-default", type=str, help="Set default version with Mike"
-    )
 
     # Parse arguments
     return parser.parse_args()
@@ -46,9 +35,6 @@ def main(docs_dir: Path):
 
     - **Dev Docs**: `python docs.py`
     - **Deploy to GitHub**: `python docs.py -gh`
-    - **Use Mike**: `python docs.py -m`
-    - **Reset Versioning**: `python docs.py -m -r`
-    - **Set Default Version**: `python docs.py -m -sd v1.0`
     """
 
     # Prepare command execution function
@@ -58,18 +44,7 @@ def main(docs_dir: Path):
     args = get_args()
 
     # Determine which command to run based on arguments
-    if args.mike:
-        mike_command = CMD_MIKE
-        if args.github:
-            mike_command += " deploy --push"
-        elif args.reset:
-            mike_command += " delete --all"
-        elif args.set_default:
-            mike_command += f" set-default {args.set_default}"
-        else:
-            mike_command += " serve"
-        command(mike_command)
-    elif args.github:
+    if args.github:
         command(CMD_DEPLOY)
     else:
         command(CMD_DEV)
