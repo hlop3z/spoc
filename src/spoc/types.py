f2c4c8b86c893cdb2c8965cf6cb1d6f6a84825f8
hlop3z <23062270+hlop3z@users.noreply.github.com>
2026-04-30 05:31:46 -0500

    Update types.py

--- src/spoc/types.py
+++ src/spoc/types.py
@@ -10,5 +10,7 @@ Provides type aliases for the framework.
 from typing import TypeAlias
 
 from .framework import Config as ConfigType
+from .components import Internal
 
 Config: TypeAlias = ConfigType
+Info: TypeAlias = Internal
